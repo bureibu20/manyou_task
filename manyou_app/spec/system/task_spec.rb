bb429a1db4d645e42034b91ce700b57bcf0d9e7e
kenya ohara <bureibu20@gmail.com>
2021-08-13 13:50:54 +0900

    rspec

--- manyou_app/spec/system/task_spec.rb
+++ manyou_app/spec/system/task_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task) { FactoryBot.create(:task) }
-  let!(:second_task) { FactoryBot.create(:second_task) }
-  before do
-    visit tasks_path
-  end
   
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -24,22 +19,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'test_title'
       end
     end
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        task = FactoryBot.create(:task, title: 'new')
-        visit tasks_path
-        task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'new'
-      end
-    end
   end
-
   describe '詳細表示機能' do
-    context '任意のタスク詳細画面に遷移した場合' do
-      it '該当タスクの内容が表示される' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
         visit task_path(id: task.id)
         expect(page).to have_content 'test_title'
-      end
-    end
+       end
+     end
   end
 end
